c8c5ee1dddaa3519ebc7f085d901a8a5c38313d7
deepjalCode <deepjalofficial@gmail.com>
2026-01-02 20:55:06 +0530

    Installation setup and enviorment setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 text-gray-200'>Today i am just seeting up things i will start project tomorrow</div>
+  )
+}
+
+export default App
